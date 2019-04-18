7c5eedcf0136af2b5fdd48c1a5bf304b3d7a7880
jason <jayboii478@gmail.com>
2019-04-18 17:08:14 -0400

    React basics, a scoreboard for a game. Need to add better documentation and will be adding more to the app as I learn more react.js!

---
+++ app.js
@@ -0,0 +1,101 @@
+const Header= (props) =>{
+    return (
+    <header>
+    <h1> {props.title}</h1>
+    <p>Total players: {props.totalPlayers}</p>
+    </header>
+ )}
+ 
+ const Player = (props)=>{
+ return (
+    <div className="player">
+      <span className='player-name'>
+        <button className='remove-player' onClick={()=> props.delete(props.id)}>x</button>
+        {props.playerName}
+      </span>
+      <Counter />
+     </div>
+ );}
+ 
+ 
+ class Counter extends React.Component {
+  state = {
+    score: 0
+  }
+ 
+  incrementScore = ()=> {
+   this.setState( prevState=> ({
+      score: prevState.score + 1
+   }))
+  }
+ 
+  decrementScore=()=>{
+    this.setState( prevState=>({
+      score: prevState.score - 1
+    }))
+ }
+ 
+  render(){
+    return(
+    <div className='counter'>
+    <button className='counter-action decrement' onClick={this.decrementScore}>-</button>
+    <span className='counter-score'>{this.state.score}</span>
+    <button className='counter-action increment' onClick={this.incrementScore}>+</button>
+ </div>
+ );}
+  }
+ 
+ 
+ class App extends React.Component{
+  state = {
+   players: [{
+      name: "Guil",
+      id: 1
+    },
+    {
+      name: "Treasure",
+      id: 2
+    },
+    {
+      name: "Ashley",
+      id: 3
+    },
+    {
+      name: "James",
+      id: 4
+    }]
+  };
+  //remove player from scoreboard
+  removePlayer=(id)=>{
+    this.setState( prevState=>({
+     players: prevState.players.filter(player => player.id !== id )
+  }))
+   
+  }
+   render(){
+    return (
+        <div className='scoreboard'>
+          <Header title='Scoreboard' totalPlayers={this.state.players.length}/>
+ 
+          {/*Players list*/}
+          {this.state.players.map(player =>
+            <Player
+             playerName={player.name}
+             id={player.id}
+             key={player.id.toString()}
+             delete={this.removePlayer} />
+             )}
+       
+        </div>
+    );
+ }}
+ 
+ 
+ 
+ 
+ ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+ )
+ 
+ 
